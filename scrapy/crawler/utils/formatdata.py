e0599dfb32cdd6568385c3ab1105de279fac80e4
Ha Minh <minhha@luxcer.com>
2016-02-21 16:40:18 +0700

    first commit

---
+++ scrapy/crawler/utils/formatdata.py
@@ -0,0 +1,42 @@
+from crawler.utils.moduleimport import *
+
+
+class FormatData:
+    @staticmethod
+    def description(self, input_value):
+        results = []
+        if isinstance(input_value, list):
+            for v in input_value:
+                if v.endswith("."):
+                    results.append(v)
+                else:
+                    results.append(v + ".")
+        return ' '.join(results)
+
+    @staticmethod
+    def title(self, input_value):
+        if isinstance(input_value, list):
+            return input_value[0]
+
+    @staticmethod
+    def post_date(self, input_value):
+        if isinstance(input_value, list):
+            return input_value[0]
+
+    @staticmethod
+    def image_urls(self, input_value):
+        results = []
+        if isinstance(input_value, list):
+            for url in input_value:
+                url = url.encode('utf-8', 'ignore')
+                if url.startswith('http'):
+                    results.append(url)
+                else:
+                    results.append(self.base_url + url)
+
+            return results
+
+    @staticmethod
+    def categories(self, input_value):
+        if isinstance(input_value, list):
+            return '/'.join(input_value)
---
+++ scrapy/crawler/utils/moduleimport.py
@@ -0,0 +1,30 @@
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors.lxmlhtml import LxmlLinkExtractor
+from scrapy.selector import HtmlXPathSelector
+from scrapy.selector.unified import SelectorList
+from scrapy.contrib.linkextractors.lxmlhtml import LxmlLinkExtractor as lxml
+
+from scrapy import FormRequest
+from scrapy import Request
+from scrapy import log
+
+from langdetect import detect
+from urlparse import urljoin
+
+from crawler.items import *
+from crawler.utils.formatdata import *
+
+import os
+import json
+import re
+import unicodedata
+import locale
+import traceback
+
+
+
+
+
+
+
+
